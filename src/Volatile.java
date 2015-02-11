73f2eb0c5029616224ef383af65d94b5e246f078
veronique <veronique@veronique-linux>
2015-02-11 14:55:37 +0100

    birdsLife : restructuration des classes et methodes

--- src/Volatile.java
+++ src/Volatile.java
@@ -2,10 +2,7 @@
 public interface Volatile {
   
   public void info();
-  public void eclore ();
   public void seDeplacer();
   public void seReproduire();
-  public void mourir ();
-  public void adulte();
   
 }
