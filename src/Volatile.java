3c7c2de2eb86590ec7c32c7001a9a6e78fd697cd
veronique <veronique@veronique-linux>
2015-02-11 14:09:55 +0100

    birdsLife : ajout methodes abs interface Volatile

--- src/Volatile.java
+++ src/Volatile.java
@@ -2,4 +2,10 @@
 public interface Volatile {
   
   public void info();
+  public void eclore ();
+  public void seDeplacer();
+  public void seReproduire();
+  public void mourir ();
+  public void vieillir();
+  
 }
